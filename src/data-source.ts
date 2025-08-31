1a3825c3677f24ba2d99d44c1fcc522f263f4b18
Noureddin Etkaidek <noor.etco@gmail.com>
2025-08-31 14:43:27 +0300

    Add TypeORM configuration and environment variable support

---
+++ src/data-source.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+
+  synchronize: false,
+});
+
+export default dataSource;
